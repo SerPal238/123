8cb1b235f8c182f837208ecdc91fbec4074698d0
SerPal238 <saplaksin@edu.hse.ru>
2022-10-13 12:57:04 +0300

    add 1.py 2.py bonus.py, done 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+from random import random
+from random import randint
+n = int(input("Размерность массива - "))
+a = [0] * n
+for i in range(n):
+    a[i] = randint(0,100) + random()
+print(a)
+c = -1
+for i in range(n):
+    if a[i] == max(a):
+        c = i
+    if c != -1 and i > c:
+        a[i] = 0
+print(a)
